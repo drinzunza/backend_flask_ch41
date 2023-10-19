620f41dab5543c68acf61579e3852c4fde55b5a4
Sergio Inzunza <sergio@asap-llc.co>
2023-10-18 19:47:22 -0700

    108 #3

---
+++ config.py
@@ -0,0 +1,7 @@
+
+me = {
+    "first_name": "Sergio",
+    "last_name": "Inzunza",
+    "email": "sinzunza@sdgku.edu",
+    "github": "https://github.com/drinzunza"
+}
--- server.py
+++ server.py
@@ -1,3 +1,37 @@
-# test change
+from flask import Flask
+from config import me
+import json
 
-print("Server") 
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    return "Hello from Flask"
+
+
+@app.get("/test")
+def test():
+    return "This is another page"
+
+
+#############################################
+#############   API    ######################
+#############################################
+
+
+@app.get("/api/version")
+def version():
+    v = {
+        "version": "1.0.0",
+        "name": "Genesis"
+    }
+    return json.dumps(v)
+
+
+
+@app.get("/api/about")
+def about():
+    return json.dumps(me)
+
+app.run(debug=True)
